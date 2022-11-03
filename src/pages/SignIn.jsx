81f98422f69970ad7f94255a8b15e2a84381b87d
olabisiolaoye10 <olabisiolaoye10@yahoo.com>
2022-11-03 10:41:43 +0100

    signin page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,14 +1,55 @@
-import { Fragment } from "react";
-import { Card } from "react-bootstrap";
+import React, { Fragment } from "react";
+import { Card, Container, Form, Row, Col, InputGroup, Button } from "react-bootstrap";
+import {AiTwotoneEye} from 'react-icons/ai';
+import '../styles/SignIn.css';
 
 const SignIn = () => {
   return (
-    <Fragment>
-      <Card>
+    <Container fluid className="h-100 d-flex align-items-center justify-content-center">
+      <Card className="cards px-4">
         {" "}
-        <Card.Title>SignIn</Card.Title>
+        <Card.Title className="text-center">Sign In</Card.Title>
+        <Card.Subtitle className="minorText text-center">Fill details to access your account</Card.Subtitle>
+        <Card.Body className="mt-4">
+          <Form>
+            <Row>
+              <Col xs={4}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Prefix</Form.Label>
+                  <Form.Select className="text-muted">
+                    <option selected>CYB</option>
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+              <Col xs={8}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Staff id</Form.Label>
+                  <Form.Control type="text" placeholder="Staff id" />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Enter password" />
+              {/* <i><AiTwotoneEye/></i> */}
+            </Form.Group>
+            <Row className="justify-content-between">
+              <Col>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  <Form.Check type="checkbox" label="Remember Me" />
+                </Form.Group>
+              </Col>
+              <Col className="text-end">
+                <Card.Link href="/" className="text-decoration-none minorText">Forgot pin?</Card.Link>
+              </Col>
+            </Row>
+            <Button className="w-100 mt-5 h6 mainBtn text-center text-white" size="lg">
+              Sign In
+            </Button>
+          </Form>
+        </Card.Body>
       </Card>
-    </Fragment>
+    </Container>
   );
 };
 
--- src/pages/StaffDashboardOverview.jsx
+++ src/pages/StaffDashboardOverview.jsx
@@ -1,5 +1,30 @@
+import React, { Fragment } from "react";
+import { Container, Row, Col, Navbar, Nav } from "react-bootstrap";
+import cyberPayLogo from '../cyberpaylogo.svg';
+import '../App.css';
+
 const StaffDashboardOverview = () => {
-  return <h2>Staff Dashboard Overview</h2>;
+  return (
+    <Fragment>
+      <Container fluid>
+          <Row>
+            <Col className="sidebar d-flex flex-column p-0" sm={12} md={2}>
+              <Navbar className="borderLight w-100 ps-3" bg="white">
+                <Navbar.Brand>
+                  <img src={cyberPayLogo}/>
+                </Navbar.Brand>
+              </Navbar>
+            </Col>
+            <Col sm={12} md={10} className="p-0">
+              <Navbar bg="white borderLight ps-3">
+                <Navbar.Text>Meal orders breakdown</Navbar.Text>
+              </Navbar>
+            </Col>
+          </Row>
+        </Container>
+    </Fragment>
+    
+  );
 };
 
 export default StaffDashboardOverview;
